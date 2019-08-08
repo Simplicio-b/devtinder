f4ab155f93fc945af3cc78d454ee3d862cde91f2
Simplício-b <simplciobaiano42@gmail.com>
2019-08-08 19:35:58 -0300

    add da dir mobile e loop na page main listando todos os dados da api

--- web/src/pages/main.js
+++ web/src/pages/main.js
@@ -1,86 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './main.css';
+
+import API from '../services/API';
 
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import deslike from '../assets/dislike.svg';
 
 export default function Main({ match }) {
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        (async function loadUsers() {
+            const response = await API.get('/devs', {
+                headers: {
+                    user: match.params.id
+                }
+            });
+            setUsers(response.data);
+        })();
+    }, [match.params.id])
+
     return (
         <div className='main-container'>
             <img src={logo} alt='Tindev' />
             <ul>
-                <li>
-                    <img src='https://avatars1.githubusercontent.com/u/41583858?v=4' alt="Tindev" />
-                    <footer>
-                        <strong>Simplicio Baiano</strong>
-                        <p>Programador e cantor de rap</p>
-                    </footer>
-
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={like}/>
-                        </button>
-                        <button type="button">
-                            <img src={deslike}/>
-                        </button>
-                    </div>
-                </li>
-            </ul>
-
-            <ul>
-                <li>
-                    <img src='https://avatars1.githubusercontent.com/u/41583858?v=4' alt="Tindev" />
-                    <footer>
-                        <strong>Simplicio Baiano</strong>
-                        <p>Programador e cantor de rap</p>
-                    </footer>
-
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={like}/>
-                        </button>
-                        <button type="button">
-                            <img src={deslike}/>
-                        </button>
-                    </div>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <img src='https://avatars1.githubusercontent.com/u/41583858?v=4' alt="Tindev" />
-                    <footer>
-                        <strong>Simplicio Baiano</strong>
-                        <p>Programador e cantor de rap</p>
-                    </footer>
-
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={like}/>
-                        </button>
-                        <button type="button">
-                            <img src={deslike}/>
-                        </button>
-                    </div>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <img src='https://avatars1.githubusercontent.com/u/41583858?v=4' alt="Tindev" />
-                    <footer>
-                        <strong>Simplicio Baiano</strong>
-                        <p>Programador e cantor de rap</p>
-                    </footer>
-
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={like}/>
-                        </button>
-                        <button type="button">
-                            <img src={deslike}/>
-                        </button>
-                    </div>
-                </li>
+                { 
+                    users.map(e => 
+                      <li key={e._id}>
+                        <img src={e.avatar} alt="Tindev" />
+                        <footer>
+                          <strong>{e.user}</strong>
+                          <p>{e.bio}</p>
+                        </footer>
+  
+                        <div className="buttons">
+                            <button type="button">
+                              <img src={like} alt="like"/>
+                            </button>
+                            <button type="button">
+                                    <img src={deslike} alt="deslike"/>
+                                </button>
+                            </div>
+                      </li> 
+                    ) 
+                }
             </ul>
         </div>
     );
