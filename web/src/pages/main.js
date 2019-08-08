d364fe3d8c69b7124da0b0cb0fb60e8f136f6863
Simplício-b <simplciobaiano42@gmail.com>
2019-08-08 18:45:03 -0300

    estilização da paigna main

--- web/src/pages/main.js
+++ web/src/pages/main.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import './main.css';
 
 import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import deslike from '../assets/dislike.svg';
 
 export default function Main({ match }) {
     return (
@@ -8,7 +11,75 @@ export default function Main({ match }) {
             <img src={logo} alt='Tindev' />
             <ul>
                 <li>
-                    <img src={logo} alt="Tindev" />
+                    <img src='https://avatars1.githubusercontent.com/u/41583858?v=4' alt="Tindev" />
+                    <footer>
+                        <strong>Simplicio Baiano</strong>
+                        <p>Programador e cantor de rap</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={like}/>
+                        </button>
+                        <button type="button">
+                            <img src={deslike}/>
+                        </button>
+                    </div>
+                </li>
+            </ul>
+
+            <ul>
+                <li>
+                    <img src='https://avatars1.githubusercontent.com/u/41583858?v=4' alt="Tindev" />
+                    <footer>
+                        <strong>Simplicio Baiano</strong>
+                        <p>Programador e cantor de rap</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={like}/>
+                        </button>
+                        <button type="button">
+                            <img src={deslike}/>
+                        </button>
+                    </div>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <img src='https://avatars1.githubusercontent.com/u/41583858?v=4' alt="Tindev" />
+                    <footer>
+                        <strong>Simplicio Baiano</strong>
+                        <p>Programador e cantor de rap</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={like}/>
+                        </button>
+                        <button type="button">
+                            <img src={deslike}/>
+                        </button>
+                    </div>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <img src='https://avatars1.githubusercontent.com/u/41583858?v=4' alt="Tindev" />
+                    <footer>
+                        <strong>Simplicio Baiano</strong>
+                        <p>Programador e cantor de rap</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={like}/>
+                        </button>
+                        <button type="button">
+                            <img src={deslike}/>
+                        </button>
+                    </div>
                 </li>
             </ul>
         </div>
