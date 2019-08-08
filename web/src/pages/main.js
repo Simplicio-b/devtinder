8def1c943e90d1ea64b9db78cd4a0b56358ded1d
Simplício-b <simplciobaiano42@gmail.com>
2019-08-08 20:12:05 -0300

    finalizacao da parte principal do projeto

--- web/src/pages/main.js
+++ web/src/pages/main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './main.css';
 
 import API from '../services/API';
@@ -22,31 +23,53 @@ export default function Main({ match }) {
         })();
     }, [match.params.id])
 
+    async function handleLike(id) {
+        API.post(`/devs/${id}/likes`, null, {
+            headers: { user: match.params.id },  
+        });
+
+        setUsers(users.filter(e => e._id !== id));
+    }
+
+    async function handleDeslike(id) {
+        API.post(`/devs/${id}/deslikes`, null, {
+            headers: { user: match.params.id },  
+        });
+    
+        setUsers(users.filter(e => e._id !== id));
+    }
+
     return (
         <div className='main-container'>
-            <img src={logo} alt='Tindev' />
-            <ul>
-                { 
-                    users.map(e => 
-                      <li key={e._id}>
-                        <img src={e.avatar} alt="Tindev" />
-                        <footer>
-                          <strong>{e.user}</strong>
-                          <p>{e.bio}</p>
-                        </footer>
-  
-                        <div className="buttons">
-                            <button type="button">
-                              <img src={like} alt="like"/>
-                            </button>
-                            <button type="button">
-                                    <img src={deslike} alt="deslike"/>
-                                </button>
-                            </div>
-                      </li> 
-                    ) 
-                }
-            </ul>
+            <Link to="/">
+                <img src={logo} alt='Tindev' />
+            </Link>
+            { users.length > 0 ? 
+                <ul>
+                {users.map(e => 
+                        <li key={e._id}>
+                          <img src={e.avatar} alt="Tindev" />
+                          <footer>
+                            <strong>{e.user}</strong>
+                            <p>{e.bio}</p>
+                          </footer>
+    
+                          <div className="buttons">
+                              <button type="button" onClick={() => handleLike(e._id)}>
+                                <img src={like} alt="like"/>
+                              </button>
+                              <button type="button" onClick={() => handleDeslike(e._id)}>
+                                  <img src={deslike} alt="deslike"/>
+                              </button>
+                          </div>
+                        </li> 
+                        )} 
+                </ul>
+                :
+                <div className="empty">
+                    Acabou :(
+                </div>
+            }
         </div>
     );
 }
