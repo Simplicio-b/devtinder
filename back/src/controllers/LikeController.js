60b568766b4de8d001094b9da5994fbe92a3cda3
Simplício-b <simplciobaiano42@gmail.com>
2019-08-10 23:22:17 -0300

    correcao de bug back-end

--- back/src/controllers/LikeController.js
+++ back/src/controllers/LikeController.js
@@ -2,7 +2,6 @@ const Dev = require('../models/Dev');
 
 module.exports = {
     async store(req, res) {
-        console.log(req.io, req.connectedUsers);
 
        const { user } = req.headers;
        const { devId } = req.params;
@@ -15,16 +14,16 @@ module.exports = {
         }
 
         if(targetDev.likes.includes(loggedDev._id)) {
-           const loggedSocket = req.connectedUsers[user];
-           const targetSocket = req.connectedUsers[devId];
+          const loggedDevSocket = req.connectedUsers[user];
+          const targetDevSocket = req.connectedUsers[devId];
 
-            if(loggedSocket) {
-               req.io.to(loggedSocket).emit('match', targetDev);
-            }
+          if(loggedDevSocket) {
+            req.io.to(loggedDevSocket).emit('match', targetDev);
+          }
 
-            if(targetSocket) {
-                req.io.to(targetSocket).emit('match', loggedSocket);
-            }
+          if(targetDevSocket) {
+            req.io.to(targetDevSocket).emit('match', loggedDev);
+          }
         }
 
         loggedDev.likes.push(targetDev._id);
